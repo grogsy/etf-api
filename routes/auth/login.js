53e81f328387b85c70a994a4eb9e49e5b0903995
Brian <a.gray.cloud@gmail.com>
2020-01-25 18:14:46 -0400

    fix issue of login not able to validate password

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -5,13 +5,20 @@ const { alreadyLoggedIn } = require("../authMiddleware");
 router.post("/", alreadyLoggedIn, async (req, res, next) => {
   try {
     const user = await User.findOne({
-      where: { email: req.body.email },
-      attributes: { exclude: ["id", "password", "salt"] }
+      where: { email: req.body.email }
     });
     if (!user || !user.correctPassword(req.body.password)) {
       res.status(401).json({ status: 401, reason: "Incorrect Email/Password" });
     } else {
-      req.login(user, err => (err ? next(err) : res.json(user)));
+      req.login(user, err =>
+        err
+          ? next(err)
+          : res.json({
+              status: 200,
+              email: user.email,
+              msg: "Logged in successfully."
+            })
+      );
     }
   } catch (error) {
     next(error);
