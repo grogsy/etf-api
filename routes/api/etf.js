2afc3d6fba038fd670bf5d159612a2ea30db8aae
Brian <a.gray.cloud@gmail.com>
2020-01-25 21:41:32 -0400

    implement more auth middlewares

--- routes/api/etf.js
+++ routes/api/etf.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { unauthorizedAccess, validateApiKey } = require("../authMiddleware");
 
 const {
   ETF,
@@ -8,45 +9,53 @@ const {
   GeoBreakdown
 } = require("../../db/models");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const etfs = await ETF.findAll({
-      // remove this after testing, this endpoint should only list etf tickers
-      // it would look like:
-      // attributes: ['ticker']
-      include: [
-        { model: FundInfo, attributes: { exclude: ["id", "etfId"] } },
-        { model: Holding, attributes: { exclude: ["id", "etfId"] } },
-        { model: Sector, attributes: { exclude: ["id", "etfId"] } },
-        { model: GeoBreakdown, attributes: { exclude: ["id", "etfId"] } }
-      ],
-      attributes: { exclude: ["id"] }
-    });
-    res.json(etfs);
-  } catch (error) {
-    next(error);
+router.get(
+  "/",
+  [unauthorizedAccess, validateApiKey],
+  async (req, res, next) => {
+    try {
+      const etfs = await ETF.findAll({
+        // remove this after testing, this endpoint should only list etf tickers
+        // it would look like:
+        // attributes: ['ticker']
+        include: [
+          { model: FundInfo, attributes: { exclude: ["id", "etfId"] } },
+          { model: Holding, attributes: { exclude: ["id", "etfId"] } },
+          { model: Sector, attributes: { exclude: ["id", "etfId"] } },
+          { model: GeoBreakdown, attributes: { exclude: ["id", "etfId"] } }
+        ],
+        attributes: { exclude: ["id"] }
+      });
+      res.json(etfs);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.get("/:ticker", async (req, res, next) => {
-  try {
-    const etf = await ETF.findOne({
-      where: {
-        ticker: req.params.ticker
-      },
-      include: [
-        { model: FundInfo, attributes: { exclude: ["id", "etfId"] } },
-        { model: Holding, attributes: { exclude: ["id", "etfId"] } },
-        { model: Sector, attributes: { exclude: ["id", "etfId"] } },
-        { model: GeoBreakdown, attributes: { exclude: ["id", "etfId"] } }
-      ],
-      attributes: { exclude: ["id"] }
-    });
+router.get(
+  "/:ticker",
+  [unauthorizedAccess, validateApiKey],
+  async (req, res, next) => {
+    try {
+      const etf = await ETF.findOne({
+        where: {
+          ticker: req.params.ticker
+        },
+        include: [
+          { model: FundInfo, attributes: { exclude: ["id", "etfId"] } },
+          { model: Holding, attributes: { exclude: ["id", "etfId"] } },
+          { model: Sector, attributes: { exclude: ["id", "etfId"] } },
+          { model: GeoBreakdown, attributes: { exclude: ["id", "etfId"] } }
+        ],
+        attributes: { exclude: ["id"] }
+      });
 
-    res.json(etf);
-  } catch (error) {
-    next(error);
+      res.json(etf);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 module.exports = router;
