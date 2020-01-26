09698337512a0014361cc70b423a286b43115716
Brian <a.gray.cloud@gmail.com>
2020-01-25 21:43:34 -0400

    add 404 handler

--- server/index.js
+++ server/index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const passport = require("passport");
+
 const db = require("../db");
 const sessionStore = new SequelizeStore({ db });
-
 const server = express();
 
 const { PORT } = require("../constants");
 const api = require("../routes/api");
 const auth = require("../routes/auth");
 
-// set up passport auth strategy
+// set up a basic passport auth strategy
 passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
   try {
@@ -25,7 +25,7 @@ passport.deserializeUser(async (id, done) => {
 // json middleware
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-// omit null values from being returned on row fetches
+// prevent null values from row fetches from being sent in the response body
 server.set("json replacer", (k, v) => (v === null ? undefined : v));
 
 // session middleware
@@ -45,6 +45,11 @@ server.use(passport.session());
 // routing middleware
 server.use("/api", api);
 server.use("/auth", auth);
+
+// 404 handler
+server.use((req, res, next) => {
+  res.status(404).json({ status: 404, msg: "Not found." });
+});
 
 // for development purposes only
 server.etc = {};
