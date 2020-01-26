75834fba51a4fede180d92cce5cd59e0e6c8b94d
Brian <a.gray.cloud@gmail.com>
2020-01-25 21:42:56 -0400

    add error handler to logout

--- routes/auth/logout.js
+++ routes/auth/logout.js
@@ -2,9 +2,13 @@ const router = require("express").Router();
 const { notLoggedIn } = require("../authMiddleware");
 
 router.post("/", notLoggedIn, (req, res) => {
-  req.logout();
-  req.session.destroy();
-  res.sendStatus(204);
+  try {
+    req.logout();
+    req.session.destroy();
+    res.status(204).json({ status: 204, msg: "Logged out successfully." });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
