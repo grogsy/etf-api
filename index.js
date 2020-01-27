1a2b35eaab939dcfdadf6c831dbe6f5513f8cb81
Brian <a.gray.cloud@gmail.com>
2020-01-27 03:51:41 -0400

    add keep alive callback

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
+const http = require("http");
 const server = require("./server");
 const db = require("./db");
 const updateETFs = require("./db/querying/updating");
-const http = require("http");
 
 const { TWENTY_FOUR_HOURS, FIVE_MINUTES } = require("./constants");
 
-db.sync(/*{ force: true }*/).then(() => {
+db.sync({ force: true }).then(() => {
   server.listen(server.etc.port, async () => {
     console.log(`up on ${server.etc.port}`);
 
     // if deployed on heroku ping page this is hosted on so it doesn't sleep
-    // setInterval(() => {
-    //   console.log("keep alive ping");
-    //   http.get("http://spdr-etf-api.herokuapp.com");
-    // }, FIVE_MINUTES);
+    setInterval(() => {
+      console.log("keep alive ping");
+      http.get("http://spdr-etf-api.herokuapp.com");
+    }, FIVE_MINUTES);
 
     setInterval(async () => {
       console.log("Server is updating data...");
